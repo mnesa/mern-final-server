40c4416c41efddc7db9494e1f258581511518fd9
mnesa <mnesa238@gmail.com>
2023-11-08 05:23:09 +0600

    class 67

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -10,6 +11,7 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0shop52.knssrag.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +30,7 @@ async function run() {
     const appointmentOptionCollection = database.collection('AppointmentOptions');
     const bookingCollection = database.collection('Bookings');
     const usersCollection = database.collection('Users')
+    const doctorCollection = database.collection('Doctors')
 
     app.get('/appointmentOptions', async (req, res) => {
       const date = req.query.date;
@@ -48,7 +51,15 @@ async function run() {
       })
 
       res.send(options)
-})
+    })
+    
+    // get only spacafic one item from database collection
+    app.get('/appointmentSpecialty', async (req, res) => {
+      const query = {};
+      const result = await appointmentOptionCollection.find(query).project({name: 1
+    }).toArray();
+    res.send(result)
+    })
 
     app.post('/bookings', async (req, res) => {
       const bookings = req.body;
@@ -89,6 +100,13 @@ async function run() {
       res.send(users)
     })
 
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({isAdmin: user?.role === 'admin'})
+    })
+
     // update specific user
     app.put('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
@@ -101,6 +119,44 @@ async function run() {
         }
       }
       const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      res.send(result)
+    })
+
+    // Doctor
+    app.get('/doctors', async (req, res) => {
+      const query = {};
+      const doctors = await doctorCollection.find(query).toArray();
+      res.send(doctors)
+})
+
+    // app.post('/doctors', async (req, res) => {
+    //   const doctor = req.body;
+    //   const result = await doctorCollection.insertOne(doctor);
+    //   res.send(result)
+    // })
+    app.post('/doctors', async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+
+      const pic = req.files.image;
+      const picData = pic.data;
+      const enCodedPic = picData.toString('base64');
+      const imageBuffer = Buffer.from(enCodedPic, 'base64');
+      const doctor = {
+        name, 
+        email,
+        image: imageBuffer
+      }
+     
+
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result)
+    })
+
+    app.delete('/doctors/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await doctorCollection.deleteOne(filter);
       res.send(result)
     })
 
